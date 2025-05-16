276a4840f04353c23832ed309e10c5bf93c57934
Hetavi Modi <hetavimodi29@gmail.com>
2025-05-16 17:43:22 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const path = require('path');
 const port = 3000;
 
 const Image = require('./models/imagemodel');
-mongoose.connect('mongodb://localhost:27017/imageupload',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err)=>{
-    console.log('Error connecting to MongoDB:', err);
-});
+mongoose.connect('mongodb://localhost:27017/imageupload');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
