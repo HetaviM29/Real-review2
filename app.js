7190d47d4d98b428374cbc649332b431c145200b
Hetavi Modi <hetavimodi29@gmail.com>
2025-05-16 16:23:30 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -6,19 +6,10 @@ const path = require('path');
 const port = 3000;
 
 const Image = require('./models/imagemodel');
-mongoose.connect('mongodb://localhost:27017/imageupload',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err)=>{
-    console.log('Error connecting to MongoDB:', err);
-});
+mongoose.connect('mongodb://localhost:27017/imageupload');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-let reviews = {};
 
 const storage = multer.diskStorage({
     destination: './public/images/',
